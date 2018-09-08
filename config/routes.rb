55f016f9f01d13aae51bb9d105c01195cfcc7aca
EC2 Default User <ec2-user@ip-172-31-2-157.us-east-2.compute.internal>
2018-09-08 10:40:20 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  get 'static_pages/home'
-match '/help',    to: 'static_pages#help',    via: 'get'
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  resources :users, only: [:show]
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
